b0a2b07ff529d894a83699072057ca78549552a9
byu-int <intelligent380@gmail.com>
2024-01-13 20:01:06 +0100

    Prep1 Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep1 World!");
         
 
         
